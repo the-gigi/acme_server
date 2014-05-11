2c1b1fa32805cde83c95b9827a926ae42dd9c5c0
Gigi <gigi.sayfan@aclimalabs.com>
2014-05-11 15:49:04 -0700

    various improvements across the board

--- acme_system_tests/simulator.py
+++ acme_system_tests/simulator.py
@@ -1,6 +1,6 @@
 """Generate alien abduction and probe requests"""
 from datetime import datetime, timedelta
-
+from time import sleep
 
 class Simulator(object):
     def __init__(self, duration, delay, acme_url):
@@ -9,6 +9,14 @@ class Simulator(object):
 
         self.duration = duration
         self.delay = delay
+
+
+class Simulator(object):
+    def __init__(self, duration, delay_in_seconds, acme_url):
+        assert isinstance(duration, timedelta)
+
+        self.duration = duration
+        self.delay_in_seconds = delay_in_seconds
         self.acme_url = acme_url
 
     def run(self):
@@ -17,9 +25,10 @@ class Simulator(object):
 
         while datetime.now() < end:
             request = self._generate_request()
-            self._send_request()
+            self._send_request(request)
+            sleep(self.delay_in_seconds)
 
-    def _send_request(self):
+    def _send_request(self, request):
         raise NotImplementedError
 
     def _generate_request(self):
